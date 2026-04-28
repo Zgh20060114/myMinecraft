04ac8ab3ef02a44bacf707c87344eeb1fc282e43
zgh20060114 <2987408354@qq.com>
2026-04-28 17:36:31 +0800

    main

--- mesh/chunkMesh.py
+++ mesh/chunkMesh.py
@@ -1,30 +1,44 @@
 from typing import TYPE_CHECKING
 import numpy as np
 from mesh.baseMesh import BaseMesh
+from engine.settings import CHUNK_AREA, CHUNK_SIZE, CHUNK_VOL
 
 if TYPE_CHECKING:
     from engine.VoxelEngine import VoxelEngine
-    from world.chunk import Chunk
 
 
 class ChunkMesh(BaseMesh):
-    def __init__(self, chunk: Chunk):
-        super().__init__(chunk.engine)
-        self.sp = chunk.engine.shader_program_manage.chunk
-        self.vbo_format = "3f 3f"
-        self.vbo_attribution = ("in_position", "in_color")
+    def __init__(self, engine: VoxelEngine):
+        super().__init__(engine)
+        self.sp = engine.shader_program_manage.chunk
+        self.vbo_format = "3u1 1u1 1u1"
+        self.vbo_attribution = ("in_position", "voxel_id", "face_id")
+        self.vbo_format_size = sum(int(form[0]) for form in self.vbo_format.split(" "))
         self.vao = self.getVAO()
+        self.chunk_voxels_id = self.buildVoxelsId()
 
     def getVertexBufferDate(self) -> np.ndarray:
-        # 面剔除顶点逆时针是正面进行渲染,顺时针是背面不渲染
-        vertices = [
-            (-0.5, -0.5, 0),
-            (0.5, -0.5, 0),
-            (0.5, 0.5, 0),
-            (0.5, 0.5, 0),
-            (-0.5, 0.5, 0),
-            (-0.5, -0.5, 0),
-        ]
-        color = [(1, 0, 0), (0, 1, 0), (0, 0, 1), (0, 0, 1), (0, 1, 0), (1, 0, 0)]
-        vertex_buffer_date = np.hstack([vertices, color], dtype=np.float32)
-        return vertex_buffer_date
+        self.getChunkVBD(self.chunk_voxels_id, self.vbo_format)
+
+    def buildVoxelsId(self):
+        voxels = np.zeros(CHUNK_VOL, dtype=np.uint8)
+
+        for z in range(CHUNK_SIZE):
+            for x in range(CHUNK_SIZE):
+                for y in range(CHUNK_SIZE):
+                    voxels[z * CHUNK_AREA + x * CHUNK_SIZE + y] = 1
+        return voxels
+
+    def getChunkVBD(self, chunk_voxels_id, vbo_format_size):
+        vbd = np.zeros(
+            CHUNK_VOL * 36 * vbo_format_size, dtype=np.uint8
+        )  # TODO: 我认为应该是36
+        vbd_index = 0
+        # 其实xyz的顺序对遍历没有影响
+        for y in range(CHUNK_SIZE):
+            for z in range(CHUNK_SIZE):
+                for x in range(CHUNK_SIZE):
+                    voxel_id = self.chunk_voxels_id[x + CHUNK_SIZE * z + CHUNK_AREA * y]
+                    if not voxel_id:
+                        continue
+                    # 顶面
--- world/chunk.py
+++ world/chunk.py
@@ -1,6 +1,5 @@
 from typing import TYPE_CHECKING
 import numpy as np
-from engine.settings import CHUNK_AREA, CHUNK_SIZE, CHUNK_VOL
 from mesh.chunkMesh import ChunkMesh
 
 if TYPE_CHECKING:
@@ -10,17 +9,7 @@ if TYPE_CHECKING:
 class Chunk:
     def __init__(self, engine: VoxelEngine):
         self.engine = engine
-        self.voxels = self.buildVoxels()
-        self.chunkMesh = ChunkMesh(self)
-
-    def buildVoxels(self):
-        voxels = np.zeros(CHUNK_VOL, dtype=np.uint8)
-
-        for z in range(CHUNK_SIZE):
-            for x in range(CHUNK_SIZE):
-                for y in range(CHUNK_SIZE):
-                    voxels[z * CHUNK_AREA + x * CHUNK_SIZE + y] = 1
-        return voxels
+        self.chunkMesh = ChunkMesh(self.engine)
 
     def render(self):
         self.chunkMesh.render()
